8707a20480214964e2d0ac374aab13664ec76d8b
Ammad Sohail Karar <56234762+ammadkarar786@users.noreply.github.com>
2022-03-14 20:17:20 +0500

    commit

--- code/final/final/ohmtest.ino
+++ code/final/final/ohmtest.ino
@@ -1,5 +1,7 @@
 /*
 #include "ohm.h"
+
+#include "mult.h"
 Resistor_value r1;
 int Vin=3.3;
 int R1=1000;
@@ -12,20 +14,18 @@ pinMode(A0,INPUT);
 pinMode(D5,OUTPUT);
 pinMode(D6,OUTPUT);
 pinMode(D7,OUTPUT);
-pinMode(D2,OUTPUT);
+pinMode(D1,OUTPUT);
 }
-
 void loop() {
   // put your main code here, to run repeatedly:
 raw=analogRead(A0);
 Serial.print(raw);
-digitalWrite(D5,0);
-digitalWrite(D6,0);
-digitalWrite(D7,0);
-digitalWrite(D2,0);
+digitalWrite(D5,0);//input A
+digitalWrite(D6,0);//INPUT B
+digitalWrite(D7,0);//INPUT C
+digitalWrite(D1,0);
 r1.ohms_setup(Vin,R1,raw);
-int result=r1.calculate_ohms();
-
+char result=change_into_char(r1.calculate_ohms());
+Serial.println (result);
 delay(1000);
-}
-*/
+}*/
